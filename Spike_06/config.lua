6994ef62c2c5de3037ed74b51bd6bef6d6f8e31d
thelionroars <thelionroars1337@gmail.com>
2015-04-01 11:35:32 +1100

    Added spike 6 - open door notifier

---
+++ Spike_06/config.lua
@@ -0,0 +1,11 @@
+-- Copyright (c) 2015 by Geekscape Pty. Ltd.  Licence LGPL V3.
+
+local module = {}
+
+module.SSID = {}
+module.SSID["SSID"] = "passphrase"
+
+module.HOST = "server_ip_here"
+module.PORT = 1337 -- or a port of Sir's/Madam's choosing
+
+return module
---
+++ Spike_06/init.lua
@@ -0,0 +1,6 @@
+-- Copyright (c) 2015 by Geekscape Pty. Ltd.  Licence LGPL V3.
+
+app    = require("open_door_notifier")
+
+config = require("config")
+require("setup").start()
