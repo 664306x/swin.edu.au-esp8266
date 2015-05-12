79fce875fa30b98d4500d09eea7bc062f53a9a45
664306x <664306x@student.swin.edu.au>
2015-05-13 04:15:43 +1000

    config
    paramatize things for easier test

--- Shower/config.lua
+++ Shower/config.lua
@@ -3,10 +3,24 @@
 local module = {}
 
 module.SSID = {}
-module.SSID["ssid"] = "pasword"
-module.SSID["ssid"] = "pasword"
+module.SSID["jacko"] = "mattisawesome"
+module.SSID["jacko2.4"] = "mattisawesome"
 
 module.HOST = "10.1.1.1"
 module.PORT = 4000
+
+module.MQTTClientUser  = "user"
+module.MQTTClientPassword  = "password"
+module.MQTTHost  = "iot.eclipse.org"
+module.MQTTPort  = 1883
+module.MQTTSecure  = 0
+
+module.Alarm1 = 120
+module.Alarm2 = 240
+module.Alarm3 = 300
+
+module.debug = true
+
+
 
 return module
